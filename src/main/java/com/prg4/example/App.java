7abd3c2921a17e9f5ab1ce786d9dd37d57311164
vipul sharma <vipulpt63@gmail.com>
2025-06-03 16:17:41 +0530

    s

---
+++ src/main/java/com/prg4/example/App.java
@@ -0,0 +1,13 @@
+package com.prg4.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
